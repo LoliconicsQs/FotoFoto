95a663566238973621929bfeaa682af962bb621f
Oni <85605192+Oni145@users.noreply.github.com>
2023-11-25 11:16:28 +0800

    Update MainUI.py

--- MainUI.py
+++ MainUI.py
@@ -1,99 +1,88 @@
-import customtkinter 
-import tkinter as ctk
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import colorchooser
 from PIL import Image, ImageOps, ImageTk, ImageFilter
 from tkinter import ttk
-import os
-import tkinter.filedialog as tk_file
+import os 
 
 
 
-root = customtkinter.CTk()
-root.geometry('1000x600')
-root.title('FotoFoto')
-root.config(bg="gray")
-options_fm = tk.Frame(root, background='white')
+root = tk.Tk()
+root.geometry("1000x600")
+root.title("FotoFoto")
+root.config(bg="white")
 
-
-def switch(indicator_lb, page):
-    for child in options_fm.winfo_children():
-        if isinstance(child, ctk.Label):
-            child['bg'] = 'SystemButtonFace'
-
-    indicator_lb['bg'] = 'black'
-    for fm in main_fm.winfo_children():
-        fm.destroy()
-        root.update()
-
-    page()
-    
-
-#Buttons of the project
+pen_color = "black"
+pen_size = 5
+file_path = ""
 
 
 
-
-Pictures_btn = ctk.Button(options_fm, text='Pictures', font=('Times New Roman', 13), 
-                          bd=0, fg='black', 
-                          activebackground='#0097e8', 
-                          command=lambda: switch(indicator_lb=Pictures_indicator_lb, 
-                                                 page=Pictures_page))
-Pictures_btn.place(x=0, y=0, width=125)
-
-Pictures_indicator_lb = ctk.Label(options_fm)
-Pictures_indicator_lb.place(x=0, y=30, width=125, height=2)
-
-
-
-Pictures_frame = customtkinter.CTkFrame(master=root, corner_radius=20)
-Pictures_frame.pack(pady=10, padx=10, fill="x", expand=True)
-
-Pictures_btn1 = customtkinter.CTkButton(master=Pictures_frame,text="Add Photos", corner_radius=10, width=50, height=30, compound="right",anchor="e", font=("Arial", 20), border_spacing=10, fg_color="black",hover_color="white")
-Pictures_btn1.pack(padx=0, pady=0)
-def popup_menu(e):
-    menu_bar.tk_popup(x=e.x_root, y=e.y_root)
-
+def add_image():
+    global file_path
+    file_path = filedialog.askopenfilename(
+        initialdir="D:/codefirst.io/Tkinter Image Editor/Pictures")
+    image = Image.open(file_path)
+    width, height = int(image.width / 2), int(image.height / 2)
+    image = image.resize((width, height), Image.LANCZOS)
+    canvas.config(width=image.width, height=image.height)
+    image = ImageTk.PhotoImage(image)
+    canvas.create_image(0, 0, image=image, anchor="nw")
+canvas = tk.Canvas(root, height=60, width=500)
 
 image_list = []
 image_vars = []
 
 def display_images(index):
     image_display_lb.config(image=image_list[index][1])
+    image_display_lb = tk.Label(root)
+    image_display_lb.pack(anchor=tk.CENTER)
 
-def load_images():
-    dir_path = tk_file.askdirectory()
 
-    image_files = os.listdir(dir_path)
+
+    image_files = os.listdir(file_path)
 
     for r in range(0, len(image_files)):
 
         
         image_list.append([
-            ImageTk.PhotoImage(Image.open(dir_path + '/' + image_files[r]).resize((50, 50), Image.LANCZOS)), 
-            ImageTk.PhotoImage(Image.open(dir_path + '/' + image_files[r]).resize((500, 500),Image.LANCZOS))]) 
+            ImageTk.PhotoImage(Image.open(file_path + '/' + image_files[r]).resize((50, 50))), 
+            ImageTk.PhotoImage(Image.open(file_path + '/' + image_files[r]).resize((480, 360)))]) 
         
         image_vars.append(f'img_{r}')
     
-    
+    slider = tk.Frame(canvas)
+    canvas.create_window((10, 10), window=slider, anchor=tk.NW)
     for n in range(len(image_vars)):
         globals()[image_vars[n]] = tk.Button(slider, image=image_list[n][0], bd=0,
                                              command=lambda n=n:display_images(n))
-        globals()[image_vars[n]].pack(side=tk.LEFT)
+    globals()[image_vars[n]].pack(side=tk.LEFT)
 
 
-menu_bar = tk.Menu(root, tearoff=False)
-Pictures_btn1.bind('<Button-1>', popup_menu)
-menu_bar.add_command(label='Add pictures', command=load_images)
 
-image_display_lb = tk.Label(root)
-image_display_lb.pack(anchor=tk.CENTER)
 
-canvas = tk.Canvas(root, height=60, width=500)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 canvas.pack(side=tk.BOTTOM, fill=tk.X)
-
-x_scroll_bar = tk.Scrollbar(root, orient=tk.HORIZONTAL)
+x_scroll_bar = ttk.Scrollbar(root, orient=tk.HORIZONTAL)
 x_scroll_bar.pack(side=tk.BOTTOM, fill=tk.X)
 x_scroll_bar.config(command=canvas.xview)
 
@@ -101,75 +90,94 @@ canvas.config(xscrollcommand=x_scroll_bar.set)
 canvas.bind('<Configure>', lambda e: canvas.bbox('all'))
 
 slider = tk.Frame(canvas)
-canvas.create_window((0, 0), window=slider, anchor=tk.NW)
-
-options_fm.pack(pady=5)
-options_fm.pack_propagate(False)
-options_fm.configure(width=516, height=32)
+canvas.create_window((10, 10), window=slider, anchor=tk.NW)
 
 
-#Every page label
-def Pictures_page():
-    Pictures_page_fm = ctk.Frame(main_fm, bg="white")
-    Pictures_page_lb = ctk.Label(Pictures_page_fm, text='Pictures', font=('Times New Roman', 13), fg='black')
-    Pictures_page_fm.pack(fill=ctk.BOTH, expand=True)
+def change_color():
+    global pen_color
+    pen_color = colorchooser.askcolor(title="Select Pen Color")[1]
 
-def Edit_page(): 
-    Edit_page_fm = ctk.Frame(main_fm, bg="white")
-    Edit_page_lb = ctk.Label(Edit_page_fm, text='Edit', font=('Times New Roman', 13), fg='black')
-    Edit_page_lb.pack(pady=80)
-    Edit_page_fm.pack(fill=ctk.BOTH, expand=True)
-image_list = []
-image_vars = []
 
-def display_images(index):
-    image_display_lb.config(image=image_list[index][1])
+def change_size(size):
+    global pen_size
+    pen_size = size
 
-def load_images():
-    dir_path = tk_file.askdirectory()
 
-    image_files = os.listdir(dir_path)
-
-    for r in range(0, len(image_files)):
-
-        
-        image_list.append([
-            ImageTk.PhotoImage(Image.open(dir_path + '/' + image_files[r]).resize((50, 50), Image.LANCZOS)), 
-            ImageTk.PhotoImage(Image.open(dir_path + '/' + image_files[r]).resize((500, 500),Image.LANCZOS))]) 
-        
-        image_vars.append(f'img_{r}')
+def draw(event):
+    x1, y1 = (event.x - pen_size), (event.y - pen_size)
+    x2, y2 = (event.x + pen_size), (event.y + pen_size)
+    canvas.create_oval(x1, y1, x2, y2, fill=pen_color, outline='')
     
-    
-    for n in range(len(image_vars)):
-        globals()[image_vars[n]] = tk.Button(slider, image=image_list[n][0], bd=0,
-                                             command=lambda n=n:display_images(n))
-        globals()[image_vars[n]].pack(side=tk.LEFT)
+
+def clear_canvas():
+    canvas.delete("all")
+    canvas.create_image(0, 0, image=canvas, anchor="nw")
 
 
+def apply_filter(filter):
+    image = Image.open(file_path)
+    width, height = int(image.width / 2), int(image.height / 2)
+    image = image.resize((width, height), Image.LANCZOS)
+    if filter == "Black and White":
+        image = ImageOps.grayscale(image)
+    elif filter == "Blur":
+        image = image.filter(ImageFilter.BLUR)
+    elif filter == "Sharpen":
+        image = image.filter(ImageFilter.SHARPEN)
+    elif filter == "Smooth":
+        image = image.filter(ImageFilter.SMOOTH)
+    elif filter == "Emboss":
+        image = image.filter(ImageFilter.EMBOSS)
+    image = ImageTk.PhotoImage(image)
+    canvas.image = image
+    canvas.create_image(0, 0, image=image, anchor="nw")
 
 
-menu_bar = tk.Menu(root, tearoff=False)
-Pictures_btn1.bind('<Button-1>', popup_menu)
-menu_bar.add_command(label='Add pictures', command=load_images)
+left_frame = tk.Frame(root, width=200, height=600, bg="white")
+left_frame.pack(side="left", fill="y")
 
-image_display_lb = tk.Label(root)
-image_display_lb.pack(anchor=tk.CENTER)
+canvas = tk.Canvas(root, width=750, height=600)
+canvas.pack()
 
-canvas = tk.Canvas(root, height=60, width=500)
-canvas.pack(side=tk.BOTTOM, fill=tk.X)
+image_button = tk.Button(left_frame, text="Add Image",
+                         command=add_image, bg="white")
+image_button.pack(pady=15)
 
-x_scroll_bar = tk.Scrollbar(root, orient=tk.HORIZONTAL)
-x_scroll_bar.pack(side=tk.BOTTOM, fill=tk.X)
-x_scroll_bar.config(command=canvas.xview)
+color_button = tk.Button(
+    left_frame, text="Change Pen Color", command=change_color, bg="white")
+color_button.pack(pady=5)
 
-canvas.config(xscrollcommand=x_scroll_bar.set)
-canvas.bind('<Configure>', lambda e: canvas.bbox('all'))
+pen_size_frame = tk.Frame(left_frame, bg="white")
+pen_size_frame.pack(pady=5)
 
-slider = tk.Frame(canvas)
-canvas.create_window((0, 0), window=slider, anchor=tk.NW)
-    
-main_fm = ctk.Frame(root)
-main_fm.pack(fill=ctk.BOTH, expand=True)
+pen_size_1 = tk.Radiobutton(
+    pen_size_frame, text="Small", value=3, command=lambda: change_size(3), bg="white")
+pen_size_1.pack(side="left")
 
+pen_size_2 = tk.Radiobutton(
+    pen_size_frame, text="Medium", value=5, command=lambda: change_size(5), bg="white")
+pen_size_2.pack(side="left")
+pen_size_2.select()
+
+pen_size_3 = tk.Radiobutton(
+    pen_size_frame, text="Large", value=7, command=lambda: change_size(7), bg="white")
+pen_size_3.pack(side="left")
+
+clear_button = tk.Button(left_frame, text="Clear",
+                         command=clear_canvas, bg="#FF9797")
+clear_button.pack(pady=10)
+
+filter_label = tk.Label(left_frame, text="Select Filter", bg="white")
+filter_label.pack()
+filter_combobox = ttk.Combobox(left_frame, values=["Black and White", "Blur",
+                                             "Emboss", "Sharpen", "Smooth"])
+filter_combobox.pack()
+
+
+filter_combobox.bind("<<ComboboxSelected>>",
+                     lambda event: apply_filter(filter_combobox.get()))
+
+
+canvas.bind("<B1-Motion>", draw)
 
 root.mainloop()
